c82036ac877573bce887706d7893cda29b632fe7
Sameer Pradhan <sameerpradhan067@gmail.com>
2026-03-11 18:00:05 +0530

    aw-105310:added two lines

--- monika-intro/src/App.js
+++ monika-intro/src/App.js
@@ -5,7 +5,10 @@ function App() {
     <div className="App">
       <h1>Hello Monika</h1>
       <h1>Hello hi</h1>
-      <h2>Where Are You</h2>
+      <h2>Where Are Y</h2>
+      <h3>i like eating</h3>
+      <p>I like to eat pizza.</p>
+
     </div>
   );
 }
